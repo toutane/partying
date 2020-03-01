5370423d53d0bbd6222098cb0a30a58293801b2e
Charles-Antoine LEGER <ca@leger.email>
2020-03-01 14:38:37 +0100

    add feed-scroll-view

---
+++ src/components/Feed/FeedScrollView.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { ScrollView, Text } from "react-native";
+
+import { ThemeContext } from "../../contexts/ThemeContext";
+import { UserContext } from "../../contexts/UserContext";
+
+import PartyCard from "../Parties/PartyCard";
+
+import { PARTIES } from "./parties";
+
+export default function FeedScrollView(props) {
+  const { theme } = useContext(ThemeContext);
+  return (
+    <ScrollView style={{ marginTop: 30, marginBottom: 250 }}>
+      {PARTIES.map((e, i) => (
+        <PartyCard key={i} currentEvent={e} />
+      ))}
+    </ScrollView>
+  );
+}
