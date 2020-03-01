1b6b7c385c7ca280544818def671e3182e4b89db
Charles-Antoine LEGER <ca@leger.email>
2020-03-01 14:39:08 +0100

    create fake db for parties

---
+++ src/components/Feed/parties.js
@@ -0,0 +1,212 @@
+export const PARTIES = [
+  {
+    organizer: {
+      username: "Alex Kokai",
+      avatar: "https://www.abc.net.au/news/image/8094494-3x2-700x467.jpg"
+    },
+    event: {
+      badge: ["party🎉", "holidays🌴"],
+      name: "Friday party",
+      time: "12:00",
+      date: "24/04",
+      location: "25 Mary Street",
+      description:
+        "Inhabit hearing perhaps on ye do no. It maids decay as there he. Smallest on suitable disposed do although blessing he juvenile in."
+    },
+    participants: [
+      {
+        username: "Paola Jump",
+        avatar:
+          "https://img.freepik.com/photos-gratuite/seduisante-jeune-femme-aux-cheveux-boucles-prend-selfie-posant-regardant-camera_8353-6636.jpg?size=626&ext=jpg",
+        state: "available"
+      },
+      {
+        username: "Alex Kokai",
+        avatar: "https://www.abc.net.au/news/image/8094494-3x2-700x467.jpg",
+        state: "unavailable"
+      },
+      {
+        username: "Paola Jump",
+        avatar:
+          "https://us.123rf.com/450wm/golubovy/golubovy1903/golubovy190300377/119098073-young-woman-portrait-fake-female-smile-eyes-closed-black-and-white-headshot-.jpg?ver=6",
+        state: "available"
+      },
+      {
+        username: "Alex Kokai",
+        avatar: "https://www.abc.net.au/news/image/8094494-3x2-700x467.jpg",
+        state: "waiting"
+      },
+      {
+        username: "Alex Kokai",
+        avatar: "https://www.abc.net.au/news/image/8094494-3x2-700x467.jpg",
+        state: "unavailable"
+      },
+      {
+        username: "Alex Kokai",
+        avatar:
+          "https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2016/01/28/14540017202113.png",
+        state: "available"
+      },
+      {
+        username: "Alex Kokai",
+        avatar: "https://www.abc.net.au/news/image/8094494-3x2-700x467.jpg",
+        state: "available"
+      },
+      {
+        username: "Alex Kokai",
+        avatar: "https://www.abc.net.au/news/image/8094494-3x2-700x467.jpg",
+        state: "unavailable"
+      },
+      {
+        username: "Paola Jump",
+        avatar:
+          "https://img.freepik.com/photos-gratuite/seduisante-jeune-femme-aux-cheveux-boucles-prend-selfie-posant-regardant-camera_8353-6636.jpg?size=626&ext=jpg",
+        state: "available"
+      },
+      {
+        username: "Paola Jump",
+        avatar:
+          "https://us.123rf.com/450wm/golubovy/golubovy1903/golubovy190300377/119098073-young-woman-portrait-fake-female-smile-eyes-closed-black-and-white-headshot-.jpg?ver=6",
+        state: "available"
+      }
+    ]
+  },
+  {
+    organizer: {
+      username: "Julie Pummet",
+      avatar:
+        "https://img.freepik.com/photos-gratuite/seduisante-jeune-femme-aux-cheveux-boucles-prend-selfie-posant-regardant-camera_8353-6636.jpg?size=626&ext=jpg"
+    },
+    participants: [
+      {
+        username: "Julie Pummet",
+        avatar:
+          "https://img.freepik.com/photos-gratuite/seduisante-jeune-femme-aux-cheveux-boucles-prend-selfie-posant-regardant-camera_8353-6636.jpg?size=626&ext=jpg",
+        state: "waiting"
+      },
+      {
+        username: "Alex Kokai",
+        avatar: "https://www.abc.net.au/news/image/8094494-3x2-700x467.jpg",
+        state: "waiting"
+      },
+      {
+        username: "Paola Jump",
+        avatar:
+          "https://img.freepik.com/photos-gratuite/seduisante-jeune-femme-aux-cheveux-boucles-prend-selfie-posant-regardant-camera_8353-6636.jpg?size=626&ext=jpg",
+        state: "available"
+      },
+      {
+        username: "Alex Kokai",
+        avatar: "https://www.abc.net.au/news/image/8094494-3x2-700x467.jpg",
+        state: "unavailable"
+      },
+      {
+        username: "Alex Kokai",
+        avatar: "https://www.abc.net.au/news/image/8094494-3x2-700x467.jpg",
+        state: "available"
+      },
+      {
+        username: "Alex Kokai",
+        avatar: "https://www.abc.net.au/news/image/8094494-3x2-700x467.jpg",
+        state: "available"
+      },
+      {
+        username: "Alex Kokai",
+        avatar: "https://www.abc.net.au/news/image/8094494-3x2-700x467.jpg",
+        state: "unavailable"
+      }
+    ],
+    event: {
+      badge: ["restaurant🍕"],
+      name: "Saturday's restaurant",
+      time: "20:30",
+      date: "1/05",
+      location: "10 Downing Street",
+      description:
+        "He unaffected sympathize discovered at no am conviction principles. Girl ham very how yet hill four show !"
+    }
+  },
+  {
+    organizer: {
+      username: "Jake Harter",
+      avatar:
+        "https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2016/01/28/14540017202113.png"
+    },
+    event: {
+      badge: ["party🎉"],
+      name: "Thursday party",
+      time: "12:00",
+      date: "24/04",
+      location: "25 Mary Street",
+      description:
+        "Inhabit hearing perhaps on ye do no. It maids decay as there he. Smallest on suitable disposed do although blessing he juvenile in."
+    },
+    participants: [
+      {
+        username: "Alex Kokai",
+        avatar: "https://www.abc.net.au/news/image/8094494-3x2-700x467.jpg",
+        state: "waiting"
+      },
+      {
+        username: "Paola Jump",
+        avatar:
+          "https://img.freepik.com/photos-gratuite/seduisante-jeune-femme-aux-cheveux-boucles-prend-selfie-posant-regardant-camera_8353-6636.jpg?size=626&ext=jpg",
+        state: "waiting"
+      }
+    ]
+  },
+  {
+    organizer: {
+      username: "Paola Jump",
+      avatar:
+        "https://us.123rf.com/450wm/golubovy/golubovy1903/golubovy190300377/119098073-young-woman-portrait-fake-female-smile-eyes-closed-black-and-white-headshot-.jpg?ver=6"
+    },
+    participants: [
+      {
+        username: "Paola Jump",
+        avatar:
+          "https://img.freepik.com/photos-gratuite/seduisante-jeune-femme-aux-cheveux-boucles-prend-selfie-posant-regardant-camera_8353-6636.jpg?size=626&ext=jpg",
+        state: "available"
+      },
+      {
+        username: "Paola Jump",
+        avatar:
+          "https://us.123rf.com/450wm/golubovy/golubovy1903/golubovy190300377/119098073-young-woman-portrait-fake-female-smile-eyes-closed-black-and-white-headshot-.jpg?ver=6",
+        state: "available"
+      },
+      {
+        username: "Paola Jump",
+        avatar:
+          "https://us.123rf.com/450wm/golubovy/golubovy1903/golubovy190300377/119098073-young-woman-portrait-fake-female-smile-eyes-closed-black-and-white-headshot-.jpg?ver=6",
+        state: "available"
+      },
+      {
+        username: "Paola Jump",
+        avatar:
+          "https://img.freepik.com/photos-gratuite/seduisante-jeune-femme-aux-cheveux-boucles-prend-selfie-posant-regardant-camera_8353-6636.jpg?size=626&ext=jpg",
+        state: "available"
+      },
+      {
+        username: "Paola Jump",
+        avatar:
+          "https://us.123rf.com/450wm/golubovy/golubovy1903/golubovy190300377/119098073-young-woman-portrait-fake-female-smile-eyes-closed-black-and-white-headshot-.jpg?ver=6",
+        state: "available"
+      },
+      {
+        username: "Paola Jump",
+        avatar:
+          "https://us.123rf.com/450wm/golubovy/golubovy1903/golubovy190300377/119098073-young-woman-portrait-fake-female-smile-eyes-closed-black-and-white-headshot-.jpg?ver=6",
+        state: "available"
+      }
+    ],
+    event: {
+      badge: ["birthday🎂", "Paola👱🏼‍♀️"],
+      name: "Paola 20' birthday with her bests friends !",
+      time: "20:30",
+      date: "1/05",
+      location: "10 Downing Street",
+      description:
+        "He unaffected sympathize discovered at no am conviction principles. Girl ham very how yet hill four show !"
+    }
+  }
+];
