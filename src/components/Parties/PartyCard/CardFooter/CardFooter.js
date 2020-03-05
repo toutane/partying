b1ebbd93df72c4001dc0f526e846dcee047d6d35
Charles-Antoine LEGER <ca@leger.email>
2020-03-05 19:52:09 +0100

    add location exented ui mode

--- src/components/Parties/PartyCard/CardFooter/CardFooter.js
+++ src/components/Parties/PartyCard/CardFooter/CardFooter.js
@@ -1,33 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
 import { Item } from "./Item";
 
 export const CardFooter = props => {
+  const [locationExtended, setLocationExtended] = useState(false);
   return (
     <View
       style={{
         height: 55,
         marginTop: 5,
         flexDirection: "row",
-        justifyContent: "space-between"
+        justifyContent: "space-around"
       }}
     >
-      <Item
-        title="Time"
-        icon="clock"
-        data={props.event.event.date}
-        {...props}
-      />
-      <Item
-        title="Date"
-        icon="calendar"
-        data={props.event.event.time}
-        {...props}
-      />
+      {!locationExtended && (
+        <Item title="Time" icon="clock" data={props.event.event} {...props} />
+      )}
+      {!locationExtended && (
+        <Item
+          title="Date"
+          icon="calendar"
+          data={props.event.event}
+          {...props}
+        />
+      )}
       <Item
         title="Location"
         icon="map-pin"
         data={props.event.event.location}
+        extd={locationExtended}
+        setExtd={setLocationExtended}
         {...props}
       />
     </View>
