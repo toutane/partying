2484f50c5c0335bd599758c099e3a82cc1dc392a
Charles-Antoine LEGER <ca@leger.email>
2020-03-08 18:06:07 +0100

    add header view for party screen

---
+++ src/components/Parties/PartyView/Top/Header/HeaderView.js
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import { Animated, View, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+import { LeftView } from "./LeftView";
+import { BackButton } from "../../../../BackButton/BackButton";
+
+export const HeaderView = props => {
+  return (
+    <Animated.View
+      style={{
+        zIndex: 1,
+        backgroundColor:
+          props.theme.theme !== "light" ? props.theme.gray6 : "white",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingHorizontal: 25,
+        opacity: props.titleOpacity
+      }}
+    >
+      <LeftView {...props} />
+      <BackButton {...props} isWhiteBackground={true} />
+    </Animated.View>
+  );
+};
