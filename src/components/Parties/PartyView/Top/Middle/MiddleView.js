cc542837d3be67b0b586a1a20754b8c244be2a8a
Charles-Antoine LEGER <ca@leger.email>
2020-03-08 18:06:36 +0100

    create left header view + middle view for party view screen

---
+++ src/components/Parties/PartyView/Top/Middle/MiddleView.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const MiddleView = props => {
+  return (
+    <View>
+      <Text
+        style={{
+          fontFamily: "sf-display-bold",
+          fontSize: 35,
+          color: props.theme.fontColor
+        }}
+        numberOfLines={1}
+      >
+        {props.party.name}
+      </Text>
+      <Text
+        style={{
+          marginTop: 20,
+          fontFamily: "sf-text-regular",
+          fontSize: 15,
+          color: props.theme.fontColor,
+          lineHeight: 30,
+          textAlign: "justify"
+        }}
+      >
+        {props.party.description}
+      </Text>
+    </View>
+  );
+};
