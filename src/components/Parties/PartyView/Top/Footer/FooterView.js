f2a8ba44967a3f8b3c530c64079e523d4d39059b
Charles-Antoine LEGER <ca@leger.email>
2020-03-09 22:04:31 +0100

    add footer party view

---
+++ src/components/Parties/PartyView/Top/Footer/FooterView.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { CardFooter } from "../../../PartyCard/CardFooter/CardFooter";
+
+export const FooterView = props => {
+  return <CardFooter {...props} />;
+};
