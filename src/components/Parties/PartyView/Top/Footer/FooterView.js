e00a22715c419842b65711fa07b38461dab62c67
Charles-Antoine LEGER <ca@leger.email>
2020-03-10 20:30:51 +0100

    fix UI footer bug with optional data (interphone, entry code, house infos)

--- src/components/Parties/PartyView/Top/Footer/FooterView.js
+++ src/components/Parties/PartyView/Top/Footer/FooterView.js
@@ -1,7 +1,101 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { CardFooter } from "../../../PartyCard/CardFooter/CardFooter";
+import React, { useState } from "react";
+import { View } from "react-native";
+import { Item } from "./Item";
+import { Hr } from "../../../../hr";
 
 export const FooterView = props => {
-  return <CardFooter {...props} />;
+  return (
+    <View
+      style={{
+        marginTop: 10,
+        marginBottom: props.party.house !== undefined ? 10 : 0
+      }}
+    >
+      <View
+        style={{
+          height: 55,
+          marginBottom: 10,
+          flexDirection: "row"
+        }}
+      >
+        <Item
+          title="Time"
+          icon="clock"
+          data={`${props.party.start.time} - ${props.party.end.time}`}
+          {...props}
+        />
+        <Item
+          title="Date"
+          icon="calendar"
+          data={`${props.party.start.date} - ${props.party.end.date}`}
+          {...props}
+        />
+      </View>
+      <View
+        style={{
+          marginBottom:
+            props.party.entry_code === undefined &&
+            props.party.interphone === undefined &&
+            props.party.house !== undefined
+              ? 20
+              : props.party.entry_code !== undefined ||
+                props.party.interphone !== undefined
+              ? 20
+              : 10
+        }}
+      >
+        <Item
+          title="Location"
+          icon="map-pin"
+          data={props.party.location}
+          isFullWidth={true}
+          {...props}
+        />
+      </View>
+      {props.party.entry_code === undefined &&
+      props.party.interphone === undefined &&
+      props.party.house !== undefined ? (
+        <Hr
+          {...props}
+          style={{
+            marginBottom:
+              props.party.entry_code === undefined &&
+              props.party.interphone === undefined
+                ? 15
+                : 0
+          }}
+        />
+      ) : null}
+      {props.party.entry_code !== undefined ||
+      props.party.interphone !== undefined ? (
+        <View>
+          <Hr {...props} />
+          <View
+            style={{
+              height: 55,
+              marginTop: 10,
+              marginBottom: props.party.house !== undefined ? 10 : 0,
+              flexDirection: "row"
+            }}
+          >
+            {props.party.entry_code !== undefined && (
+              <Item title="Entry code" icon="key" data="13B06" {...props} />
+            )}
+            {props.party.interphone !== undefined && (
+              <Item title="Interphone" icon="phone" data="JOHN" {...props} />
+            )}
+          </View>
+        </View>
+      ) : null}
+      {props.party.house !== undefined && (
+        <Item
+          title="House"
+          icon="home"
+          data={props.party.house}
+          isFullWidth={true}
+          {...props}
+        />
+      )}
+    </View>
+  );
 };
