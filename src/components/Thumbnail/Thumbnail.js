2222c34dad7067f2f91eef348eefef88194bfc06
Charles-Antoine LEGER <ca@leger.email>
2020-03-11 19:14:24 +0100

    improve thumbnail component

--- src/components/Thumbnail/Thumbnail.js
+++ src/components/Thumbnail/Thumbnail.js
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../../contexts/UserContext";
 import { TouchableOpacity } from "react-native";
-
 import { Image } from "./styles";
 
 export const Thumbnail = props => {
+  const { currentUserId } = useContext(UserContext);
   return (
     <TouchableOpacity
       activeOpacity={0.5}
       onPress={() =>
-        props.user.uid === props.crntUserId
+        props.user.user_id === currentUserId
           ? props.navigation.navigate("Profile")
           : props.navigation.navigate("UserView", {
-              user: props.party.organizer
+              user: props.user
             })
       }
     >
