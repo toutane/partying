14dc419991ba97e042aa8717d16e027d43518cae
Charles-Antoine LEGER <ca@leger.email>
2020-03-15 18:58:46 +0100

    create options bar for account header

---
+++ src/components/AccountView/Header/OptionsBar/OptionsBar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { TouchableOpacity, View, Text } from "react-native";
+import { Button } from "./styles";
+import { screenWidth } from "../../../../utils/dimensions";
+
+export const OptionsBar = props => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 20,
+        paddingHorizontal: 25
+      }}
+    >
+      <Button
+        {...props}
+        style={{ width: screenWidth / 2 - 32.5 }}
+        activeOpacity={0.5}
+      >
+        <Text
+          style={{
+            color: props.theme.backgroundColor,
+            fontSize: 15,
+            fontFamily: "sf-text-semibold"
+          }}
+        >
+          Edit profile
+        </Text>
+      </Button>
+      <Button
+        {...props}
+        style={{ width: screenWidth / 2 - 32.5 }}
+        activeOpacity={0.5}
+      >
+        <Text
+          style={{
+            color: props.theme.backgroundColor,
+            fontSize: 15,
+            fontFamily: "sf-text-semibold"
+          }}
+        >
+          Settings
+        </Text>
+      </Button>
+    </View>
+  );
+};
