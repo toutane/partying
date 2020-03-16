8f9f8589aefcd8d9c0955e44a7b76059299f948f
Charles-Antoine LEGER <ca@leger.email>
2020-03-16 22:36:01 +0100

    add debug button for party id

---
+++ src/components/Parties/PartyCard/CardHeader/debug_id_button.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+export const Debug_id_button = props => {
+  return (
+    <TouchableOpacity
+      style={{ height: 30, width: 30, marginRight: 5 }}
+      onPress={() => alert(`debug : doc id : ${props.party.party_id}`)}
+    >
+      <Feather
+        color={
+          props.theme.theme === "light" ? props.theme.gray6 : props.theme.gray7
+        }
+        size={25}
+        name="file-text"
+      />
+    </TouchableOpacity>
+  );
+};
