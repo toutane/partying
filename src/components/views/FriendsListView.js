604a3764ad005f1b16268b709b179e9f885d6541
Charles-Antoine LEGER <ca@leger.email>
2020-03-19 22:50:07 +0100

    put infinite scroll for friends flat list into friends context... yehh!

--- src/components/views/FriendsListView.js
+++ src/components/views/FriendsListView.js
@@ -10,107 +10,27 @@ import { FriendsFlatList } from "../User/Friends/FriendsList/FriendsFlatList";
 
 export default FriendsListView = props => {
   const { theme } = useContext(ThemeContext);
-  const { loadFriends } = useContext(FriendsContext);
+  const {
+    friends,
+    loading,
+    refreshing,
+    retrieveData,
+    retrieveMore
+  } = useContext(FriendsContext);
 
   const [scrollY, setScrollY] = useState(new Animated.Value(100));
   const [user, setUser] = useState(props.navigation.getParam("user"));
 
-  const [friends, setFriends] = useState([]);
-  const [limit, setLimit] = useState(2);
-  const [lastVisibleState, setLastVisible] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [refreshing, setRefreshing] = useState(false);
-
   useEffect(() => {
     try {
-      // Cloud Firestore: Initial Query
       retrieveData(user.user_id);
     } catch (error) {
       console.log(error);
     }
   }, []);
 
-  retrieveData = async user_id => {
-    try {
-      // Set State: Loading
-      setLoading(true);
-      console.log("Retrieving Data");
-      // Cloud Firestore: Query
-      let initialQuery = await firebase.db
-        .collection("users")
-        .doc(user_id)
-        .collection("friends")
-        .where("id", "<=", 4)
-        .orderBy("id")
-        .limit(limit);
-      // Cloud Firestore: Query Snapshot
-      let documentSnapshots = await initialQuery.get();
-      // Cloud Firestore: Document Data
-      let documentData = documentSnapshots.docs.map(document =>
-        document.data()
-      );
-      // Cloud Firestore: Last Visible Document (Document ID To Start From For Proceeding Queries)
-      let lastVisible = documentData[documentData.length - 1].id;
-      // Set State
-      setFriends(documentData);
-      setLastVisible(lastVisible);
-      setLoading(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  // Retrieve More
-  retrieveMore = async user_id => {
-    try {
-      // Set State: Refreshing
-      setRefreshing(true);
-      console.log("Retrieving additional Data");
-      // Cloud Firestore: Query (Additional Query)
-      let additionalQuery = await firebase.db
-        .collection("users")
-        .doc(user_id)
-        .collection("friends")
-        .where("id", "<=", 4)
-        .orderBy("id")
-        .startAfter(lastVisibleState)
-        .limit(limit);
-      // Cloud Firestore: Query Snapshot
-      let documentSnapshots = await additionalQuery.get();
-      // Cloud Firestore: Document Data
-      let documentData = documentSnapshots.docs.map(document =>
-        document.data()
-      );
-      // Cloud Firestore: Last Visible Document (Document ID To Start From For Proceeding Queries)
-      let lastVisible = documentData[documentData.length - 1].id;
-      // Set State
-      setFriends([...friends, ...documentData]);
-      setLastVisible(lastVisible);
-      setRefreshing(false);
-      //
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  // useEffect(() => loadFriends(user.user_id).then(res => setFriends(res)), []);
   return (
     <View style={{ backgroundColor: theme.backgroundColor }}>
-      {/* <ScrollView
-        style={{
-          zIndex: 1,
-          height: screenHeight,
-          
-        }}
-        onScroll={Animated.event([
-          { nativeEvent: { contentOffset: { y: scrollY } } }
-        ])}
-        contentContainerStyle={{
-          marginTop: 46 + useSafeArea().top
-        }}
-        scrollEventThrottle={16}
-        snapToAlignment={"start"}
-        snapToInterval={60}
-      > */}
       <FriendsFlatList
         theme={theme}
         user={user}
@@ -120,7 +40,6 @@ export default FriendsListView = props => {
         loading={loading}
         {...props}
       />
-      {/* </ScrollView> */}
       <DefaultHeader
         {...props}
         scrollY={scrollY}
--- src/contexts/FriendsContext.js
+++ src/contexts/FriendsContext.js
@@ -6,21 +6,81 @@ const FriendsContext = React.createContext();
 const { Provider } = FriendsContext;
 
 const FriendsProvider = props => {
-  async function loadFriends(user_id) {
-    const friends = await firebase.db
-      .collection("users")
-      .doc(user_id)
-      .collection("friends")
-      .get();
-    return friends.docs.map(doc => ({
-      ...doc.data()
-    }));
-  }
+  const [friends, setFriends] = useState([]);
+  const [limit, setLimit] = useState(2);
+  const [lastVisibleState, setLastVisible] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+  retrieveData = async user_id => {
+    try {
+      // Set State: Loading
+      setLoading(true);
+      console.log("Retrieving Data");
+      // Cloud Firestore: Query
+      let initialQuery = await firebase.db
+        .collection("users")
+        .doc(user_id)
+        .collection("friends")
+        .where("id", "<=", 4)
+        .orderBy("id")
+        .limit(limit);
+      // Cloud Firestore: Query Snapshot
+      let documentSnapshots = await initialQuery.get();
+      // Cloud Firestore: Document Data
+      let documentData = documentSnapshots.docs.map(document =>
+        document.data()
+      );
+      // Cloud Firestore: Last Visible Document (Document ID To Start From For Proceeding Queries)
+      let lastVisible = documentData[documentData.length - 1].id;
+      // Set State
+      setFriends(documentData);
+      setLastVisible(lastVisible);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  // Retrieve More
+  retrieveMore = async user_id => {
+    try {
+      // Set State: Refreshing
+      setRefreshing(true);
+      console.log("Retrieving additional Data");
+      // Cloud Firestore: Query (Additional Query)
+      let additionalQuery = await firebase.db
+        .collection("users")
+        .doc(user_id)
+        .collection("friends")
+        .where("id", "<=", 4)
+        .orderBy("id")
+        .startAfter(lastVisibleState)
+        .limit(limit);
+      // Cloud Firestore: Query Snapshot
+      let documentSnapshots = await additionalQuery.get();
+      // Cloud Firestore: Document Data
+      let documentData = documentSnapshots.docs.map(document =>
+        document.data()
+      );
+      // Cloud Firestore: Last Visible Document (Document ID To Start From For Proceeding Queries)
+      let lastVisible = documentData[documentData.length - 1].id;
+      // Set State
+      setFriends([...friends, ...documentData]);
+      setLastVisible(lastVisible);
+      setRefreshing(false);
+      //
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Provider
       value={{
-        loadFriends
+        friends,
+        loading,
+        refreshing,
+        retrieveData,
+        retrieveMore
       }}
     >
       {props.children}
