7a2d6bfbd041a213c68cef23631940e207eb6fcf
Charles-Antoine LEGER <ca@leger.email>
2020-03-20 15:36:15 +0100

    finish friends list item

--- src/components/User/Friends/FriendsList/Item.js
+++ src/components/User/Friends/FriendsList/Item.js
@@ -1,12 +1,38 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { Thumbnail } from "../../../Thumbnail/Thumbnail";
+import { screenWidth } from "../../../../utils/dimensions";
 
 export const Item = props => {
   return (
-    <View style={{ marginBottom: 10 }}>
-      <Text
-        style={{ color: props.theme.fontColor }}
-      >{`ID : ${props.friend.user_id} ( ${props.friend.id} )`}</Text>
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "row",
+        marginTop: 20
+      }}
+    >
+      <Thumbnail user={props.friend} {...props} uri={props.friend.avatar} />
+      <View style={{ flex: 1, marginLeft: 15, justifyContent: "center" }}>
+        <Text
+          style={{
+            color: props.theme.fontColor,
+            fontSize: 17,
+            fontFamily: "sf-text-semibold"
+          }}
+        >
+          {props.friend.username}
+        </Text>
+        <Text
+          style={{
+            color: props.theme.gray,
+            fontSize: 13,
+            fontFamily: "sf-text-medium"
+          }}
+        >
+          {props.friend.name}
+        </Text>
+      </View>
     </View>
   );
 };
