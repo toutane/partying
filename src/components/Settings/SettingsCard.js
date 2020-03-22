9691c194b3bd174b20d7a85b1bec28fe05e54859
Charles-Antoine LEGER <ca@leger.email>
2020-03-22 19:50:29 +0100

    update hr styles

--- src/components/Settings/SettingsCard.js
+++ src/components/Settings/SettingsCard.js
@@ -1,57 +1,13 @@
 import React from "react";
 import { View, TouchableOpacity, Text } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { AppearanceItem } from "./Appearance/AppearanceItem";
+import { PushNotificationsItem } from "./PushNotifications/PushNotificationsItem";
 
 export const SettingsCard = props => {
   return (
     <View style={{ marginTop: 40 }}>
-      <TouchableOpacity
-        onPress={() => props.navigation.navigate("Appearance")}
-        activeOpacity={0.5}
-        style={{
-          borderTopLeftRadius: 13,
-          borderTopRightRadius: 13,
-          backgroundColor:
-            props.theme.theme !== "light" ? props.theme.gray6 : "white",
-          paddingHorizontal: 20,
-          paddingVertical: 15
-        }}
-      >
-        <View
-          style={{
-            justifyContent: "space-between",
-            flexDirection: "row"
-          }}
-        >
-          <Text
-            style={{
-              color: props.theme.fontColor,
-              fontSize: 17,
-              fontFamily: "sf-text-regular"
-            }}
-          >
-            Appearance
-          </Text>
-          <View style={{ flexDirection: "row" }}>
-            <Text
-              style={{
-                color: props.theme.gray4,
-                fontSize: 17,
-                fontFamily: "sf-text-regular"
-              }}
-            >
-              {props.theme.theme.charAt(0).toUpperCase() +
-                props.theme.theme.slice(1)}{" "}
-            </Text>
-            <Ionicons
-              style={{ marginLeft: 5 }}
-              name="ios-arrow-forward"
-              size={20}
-              color={props.theme.gray4}
-            />
-          </View>
-        </View>
-      </TouchableOpacity>
+      <AppearanceItem {...props} />
+      <PushNotificationsItem {...props} />
     </View>
   );
 };
--- src/components/hr/index.js
+++ src/components/hr/index.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Hr = styled.View`
   border-color: ${({ theme }) => theme.gray5};
-  border-width: 0.6px;
+  border-width: 0.5px;
 `;
 
 export { Hr };
