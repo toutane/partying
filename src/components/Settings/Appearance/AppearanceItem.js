495db79b2357ddd4d50b8d5562b3445c8358c84b
Charles-Antoine LEGER <ca@leger.email>
2020-03-22 19:50:21 +0100

    create appearance item for settings card

---
+++ src/components/Settings/Appearance/AppearanceItem.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+export const AppearanceItem = props => {
+  return (
+    <TouchableOpacity
+      onPress={() => props.navigation.navigate("Appearance")}
+      activeOpacity={0.5}
+      style={{
+        borderTopLeftRadius: 13,
+        borderTopRightRadius: 13,
+        backgroundColor:
+          props.theme.theme !== "light" ? props.theme.gray6 : "white",
+        paddingHorizontal: 20,
+        paddingVertical: 15
+      }}
+    >
+      <View
+        style={{
+          justifyContent: "space-between",
+          flexDirection: "row"
+        }}
+      >
+        <Text
+          style={{
+            color: props.theme.fontColor,
+            fontSize: 17,
+            fontFamily: "sf-text-regular"
+          }}
+        >
+          Appearance
+        </Text>
+        <View style={{ flexDirection: "row" }}>
+          <Text
+            style={{
+              color: props.theme.gray4,
+              fontSize: 17,
+              fontFamily: "sf-text-regular"
+            }}
+          >
+            {props.theme.theme.charAt(0).toUpperCase() +
+              props.theme.theme.slice(1)}{" "}
+          </Text>
+          <Ionicons
+            style={{ marginLeft: 5 }}
+            name="ios-arrow-forward"
+            size={20}
+            color={props.theme.gray4}
+          />
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
