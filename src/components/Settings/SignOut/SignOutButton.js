fd54d21939bd4489aae1e670f67cbb07e2c3f257
Charles-Antoine LEGER <ca@leger.email>
2020-03-22 19:33:06 +0100

    add sign out btn

---
+++ src/components/Settings/SignOut/SignOutButton.js
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import { View, TouchableOpacity, Text } from "react-native";
+
+import { AuthContext } from "../../../contexts/AuthContext";
+
+export const SignOutButton = props => {
+  const { logout } = useContext(AuthContext);
+  return (
+    <View style={{ marginTop: 40 }}>
+      <TouchableOpacity
+        onPress={() => logout()}
+        activeOpacity={0.5}
+        style={{
+          borderRadius: 13,
+          backgroundColor:
+            props.theme.theme !== "light" ? props.theme.gray6 : "white",
+          paddingHorizontal: 20,
+          paddingVertical: 15
+        }}
+      >
+        <Text
+          style={{
+            color: props.theme.red,
+            fontSize: 17,
+            fontFamily: "sf-text-regular"
+          }}
+        >
+          Sign out
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
