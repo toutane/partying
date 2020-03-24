8d883ef5a721623dbfb736b04ed04b34a0f55983
Charles-Antoine LEGER <ca@leger.email>
2020-03-24 22:08:13 +0100

    implement local stored theme state system

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useColorScheme } from "react-native-appearance";
+import { AsyncStorage } from "react-native";
 
 import { darkTheme } from "../themes/darkTheme";
 import { lightTheme } from "../themes/lightTheme";
@@ -8,18 +9,59 @@ const ThemeContext = React.createContext();
 const { Provider } = ThemeContext;
 
 const ThemeProvider = props => {
+  const [colorScheme, setColorScheme] = useState(
+    useColorScheme() === "dark" ? darkTheme : lightTheme
+  );
   const [theme, setTheme] = useState(
     useColorScheme() === "dark" ? darkTheme : lightTheme
   );
-  function switchTheme() {
-    setTheme(theme.theme === "light" ? darkTheme : lightTheme);
-  }
+  const [themeState, setThemeState] = useState("automatic");
+
+  const _storeData = async themeState => {
+    try {
+      await AsyncStorage.setItem("themeState", themeState);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const _retrieveData = async () => {
+    try {
+      const value = await AsyncStorage.getItem("themeState");
+      if (value !== null) {
+        setThemeState(value);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    _retrieveData();
+  }, []);
+
+  useEffect(() => {
+    themeState === "automatic"
+      ? setTheme(colorScheme)
+      : themeState === "light"
+      ? setTheme(lightTheme)
+      : setTheme(darkTheme);
+  }, [themeState]);
+
+  useEffect(() => {
+    _storeData(themeState);
+  }, [themeState]);
+
+  // useEffect(() => {
+  //   console.log(themeState);
+  // }, [themeState]);
 
   return (
     <Provider
       value={{
         theme,
-        switchTheme
+        themeState,
+        setThemeState
       }}
     >
       {props.children}
