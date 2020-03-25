997a7880d2a51c4acff0e83c2dd3e35dcda6b228
Charles-Antoine LEGER <ca@leger.email>
2020-03-25 21:42:07 +0100

    set up appState check and notifications permissions status check

--- src/contexts/PushNotificationsContext.js
+++ src/contexts/PushNotificationsContext.js
@@ -1,13 +1,40 @@
 import React, { useState, useEffect } from "react";
-import { AsyncStorage } from "react-native";
+import { AppState, AsyncStorage } from "react-native";
+import * as Permissions from "expo-permissions";
 
 const PushNotificationsContext = React.createContext();
 const { Provider } = PushNotificationsContext;
 
 const PushNotificationsProvider = props => {
+  const [appState, setAppState] = useState(AppState.currentState);
   const [isPushNotifActive, setIsPushNotifActive] = useState(false);
   const [isDirectMentionsActive, setIsDirectMentionsActive] = useState(false);
 
+  useEffect(() => {
+    checkNotificationsPermissionsStatus();
+  }, [appState]);
+
+  useEffect(() => {
+    console.log(appState);
+  }, [appState]);
+
+  useEffect(() => {
+    AppState.addEventListener("change", _handleAppStateChange);
+  }, []);
+
+  _handleAppStateChange = nextAppState => {
+    // if (appState.match(/inactive|background/) && nextAppState === "active") {
+    //   console.log("App has come to the foreground!");
+    // }
+    setAppState(nextAppState);
+  };
+
+  async function checkNotificationsPermissionsStatus() {
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    status === "granted"
+      ? setIsPushNotifActive(true)
+      : (setIsPushNotifActive(false), setIsDirectMentionsActive(false));
+  }
   const _storeData = async isDirectMentionsActive => {
     try {
       await AsyncStorage.setItem(
