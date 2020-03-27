a64a3cf7caad61f7cfc120a139cf7bd02865b354
Charles-Antoine LEGER <ca@leger.email>
2020-03-27 23:50:51 +0100

    update name card component

--- src/components/Parties/NewParty/NameCard/NameCard.js
+++ src/components/Parties/NewParty/NameCard/NameCard.js
@@ -1,9 +1,11 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 import { Card } from "../../../Card/styles";
 import { Inputs } from "./Inputs";
+import { Hr } from "../../../hr";
 
 export const NameCard = props => {
+  const [showInfo, setShwoInfo] = useState(false);
   return (
     <View style={{ marginTop: 30, marginBottom: 30, paddingHorizontal: 25 }}>
       <Card {...props}>
@@ -16,6 +18,35 @@ export const NameCard = props => {
         >
           NAME YOUR EVENT :
         </Text> */}
+        <Text
+          style={{
+            marginBottom: 5,
+            fontFamily: "sf-text-semibold",
+            fontSize: 17,
+            color: props.theme.fontColor
+          }}
+        >
+          Name your party
+        </Text>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={() => setShwoInfo(prvState => !prvState)}
+        >
+          <Text
+            style={{
+              marginBottom: 15,
+              fontFamily: "sf-text-regular",
+              fontSize: 13,
+              color: props.theme.gray2
+            }}
+            numberOfLines={showInfo ? 10 : 1}
+          >
+            Give a name and description to your new event to let your guests
+            know what it is. You can also pass there information about your
+            party (is it disguised for example ? etc).
+          </Text>
+        </TouchableOpacity>
+        <Hr {...props} style={{ marginBottom: 15 }} />
         <Inputs {...props} />
       </Card>
     </View>
