44d47b9ca4b4dd34bf223954c8391d6a8554817e
Charles-Antoine LEGER <ca@leger.email>
2020-03-27 20:33:48 +0100

    update new party screen

--- src/components/Parties/NewParty/NewPartyScreen.js
+++ src/components/Parties/NewParty/NewPartyScreen.js
@@ -3,12 +3,16 @@ import { Animated, View } from "react-native";
 
 import { HeaderView } from "./Header/HeaderView";
 import { MiddleView } from "./Middle/MiddleView";
+import { NameCard } from "./NameCard/NameCard";
+import { TimesCard } from "./TimesCard/TimesCard";
 
 export default function NewPartyScreen(props) {
   return (
-    <View style={{ marginBottom: 100 }}>
+    <View>
       {/* <HeaderView {...props} /> */}
-      <MiddleView {...props} />
+      {/* <MiddleView {...props} /> */}
+      <NameCard {...props} />
+      <TimesCard {...props} />
     </View>
   );
 }
