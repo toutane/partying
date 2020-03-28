b9632ad355887f27cc47f5f3c0db9caa30f59387
Charles-Antoine LEGER <ca@leger.email>
2020-03-28 22:14:16 +0100

    add time input component

--- src/components/Parties/NewParty/TimesCard/TimeInput.js
+++ src/components/Parties/NewParty/TimesCard/TimeInput.js
@@ -1,7 +1,6 @@
 import React, { useState, useContext } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import Icon from "react-native-feather1s";
-import DateTimePicker from "@react-native-community/datetimepicker";
 import { CreatePartyContext } from "../../../../contexts/CreatePartyContext";
 
 const moment = require("moment");
@@ -10,40 +9,87 @@ export const TimeInput = props => {
   const { partyStarts, setPartyStarts, partyEnds, setPartyEnds } = useContext(
     CreatePartyContext
   );
-  const [show, setShow] = useState(false);
-
   return (
-    <View style={{ flexDirection: "row", alignItems: "center", marginTop: 10 }}>
-      <Icon
-        name="clock"
-        size={30}
-        color={props.theme.gray4}
-        style={{ marginRight: 5 }}
-      />
-      <View style={{ flexDirection: "column", flex: 1 }}>
-        <Text
-          style={{
-            fontFamily: "sf-text-medium",
-            fontSize: 15,
-            color: props.theme.gray4,
-            marginBottom: 3
-          }}
-        >
-          Time
-        </Text>
-        <Text
-          style={{
-            fontFamily: "sf-text-semibold",
-            fontSize: 17,
-            color: props.theme.fontColor
-          }}
-          numberOfLines={2}
-        >
-          {`From ${moment(partyStarts.date).format("dddd MMMM Do")} to ${moment(
-            partyEnds.date
-          ).format("dddd MMMM Do")}`}
-        </Text>
+    <View
+      style={{
+        height: 85
+      }}
+    >
+      <Text
+        style={{
+          fontFamily: "sf-text-medium",
+          fontSize: 17,
+          color: props.theme.fontColor,
+          marginBottom: 15
+        }}
+      >
+        Time :
+      </Text>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          height: 40
+        }}
+      >
+        <Icon
+          name="clock"
+          size={30}
+          color={props.theme.gray4}
+          style={{ marginRight: 10 }}
+        />
+        <View style={{ flexDirection: "row" }}>
+          <View style={{ flexDirection: "column", marginTop: 3 }}>
+            <Text
+              style={{
+                fontFamily: "sf-text-medium",
+                fontSize: 15,
+                color: props.theme.gray4,
+                marginBottom: 10
+              }}
+            >
+              from
+            </Text>
+            <Text
+              style={{
+                fontFamily: "sf-text-medium",
+                fontSize: 15,
+                color: props.theme.gray4
+              }}
+            >
+              to
+            </Text>
+          </View>
+          <View
+            style={{ flexDirection: "column", marginLeft: 10, marginTop: 1 }}
+          >
+            <Text
+              style={{
+                fontFamily: "sf-text-semibold",
+                fontSize: 17,
+                color: props.theme.fontColor,
+                marginBottom: 10
+              }}
+              numberOfLines={2}
+            >
+              {moment(partyStarts.time).format("LT")}
+            </Text>
+
+            <Text
+              style={{
+                fontFamily: "sf-text-semibold",
+                fontSize: 17,
+                color: props.theme.fontColor
+              }}
+              numberOfLines={2}
+            >
+              {moment(partyEnds.time).format("LT")}
+            </Text>
+          </View>
+        </View>
       </View>
     </View>
   );
 };
+
+// - [ ] ajouter la différenciation : event de plus de une journée ou nom (pour faire apparaitre ou non la selection du jour de fin de l'event) : si l'event dure plusieurs jours, afficher dans le coin de la card e nombre de jours.
