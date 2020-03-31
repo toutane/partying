21ae61287cf4010ade4eed0d245a45aa32a3270d
Charles-Antoine LEGER <ca@leger.email>
2020-03-31 23:11:59 +0200

    create collapse view component

---
+++ src/components/CollapseView/CollapseView.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import { Animated, View } from "react-native";
+
+export const CollapseView = props => {
+  const [heightAnim] = useState(new Animated.Value(props.minHeight));
+
+  useEffect(() => {
+    console.log(props.collapse);
+    !props.collapse
+      ? Animated.timing(heightAnim, {
+          toValue: props.minHeight,
+          duration: props.duration
+        }).start()
+      : Animated.timing(heightAnim, {
+          toValue: props.maxHeight,
+          duration: props.duration
+        }).start();
+  }, [props.collapse]);
+
+  return (
+    <Animated.View
+      style={{
+        height: heightAnim
+      }}
+    >
+      {props.children}
+    </Animated.View>
+  );
+};
--- src/components/Parties/NewParty/NameCard/NameCard.js
+++ src/components/Parties/NewParty/NameCard/NameCard.js
@@ -3,14 +3,29 @@ import { View, Text, TouchableOpacity } from "react-native";
 import { Card } from "../../../Card/styles";
 import { Inputs } from "./Inputs";
 import { Hr } from "../../../hr";
+import { CollapseView } from "../../../CollapseView/CollapseView";
 
 export const NameCard = props => {
   const [showInfo, setShowInfo] = useState(false);
+  const renderview = () => {
+    <Text
+      style={{
+        fontFamily: "sf-text-regular",
+        fontSize: 13,
+        color: props.theme.gray2
+      }}
+      // numberOfLines={10}
+    >
+      Give a name and description to your new event to let your guests know what
+      it is. You can also pass there information about your party (is it
+      disguised for example ? etc).
+    </Text>;
+  };
   return (
     <View style={{ marginTop: 30, marginBottom: 30, paddingHorizontal: 25 }}>
       <Card {...props}>
         <TouchableOpacity
-          activeOpacity={0.7}
+          activeOpacity={1}
           onPress={() => setShowInfo(prvState => !prvState)}
         >
           <Text
@@ -23,19 +38,26 @@ export const NameCard = props => {
           >
             Name your party
           </Text>
-          <Text
-            style={{
-              marginBottom: 15,
-              fontFamily: "sf-text-regular",
-              fontSize: 13,
-              color: props.theme.gray2
-            }}
-            numberOfLines={showInfo ? 10 : 1}
+          <CollapseView
+            collapse={showInfo}
+            minHeight={30}
+            maxHeight={80}
+            duration={150}
+            {...props}
           >
-            Give a name and description to your new event to let your guests
-            know what it is. You can also pass there information about your
-            party (is it disguised for example ? etc).
-          </Text>
+            <Text
+              style={{
+                fontFamily: "sf-text-regular",
+                fontSize: 13,
+                color: props.theme.gray2
+              }}
+              // numberOfLines={10}
+            >
+              Give a name and description to your new event to let your guests
+              know what it is. You can also pass there information about your
+              party (is it disguised for example ? etc).
+            </Text>
+          </CollapseView>
         </TouchableOpacity>
         <Hr {...props} style={{ marginBottom: 15 }} />
         <Inputs {...props} />
