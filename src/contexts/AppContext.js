7690f6298d577b3ee8c9646ee24ac104ad31e3d9
Charles-Antoine LEGER <ca@leger.email>
2020-04-01 22:33:59 +0200

    create appContext for app state

---
+++ src/contexts/AppContext.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import { AppState } from "react-native";
+
+const AppContext = React.createContext();
+const { Provider } = AppContext;
+
+const AppProvider = props => {
+  const [appState, setAppState] = useState(AppState.currentState);
+
+  useEffect(() => console.log(appState), [appState]);
+
+  useEffect(() => {
+    AppState.addEventListener("change", _handleAppStateChange);
+  }, []);
+
+  _handleAppStateChange = nextAppState => {
+    setAppState(nextAppState);
+  };
+
+  return <Provider value={{ appState }}>{props.children}</Provider>;
+};
+
+export { AppProvider, AppContext };
