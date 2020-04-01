d1bc1f787d20cda50c88325c3b43de355e0faca7
Charles-Antoine LEGER <ca@leger.email>
2020-04-01 22:34:18 +0200

    update map component

--- src/components/Parties/NewParty/LocationCard/Map.js
+++ src/components/Parties/NewParty/LocationCard/Map.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, Text } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
+import { LocationContext } from "../../../../contexts/LocationContext";
 
 export const Map = props => {
+  const { currentPosition, currentPositionAddress } = useContext(
+    LocationContext
+  );
+
   return (
     <View>
-      <MapView
-        style={{ height: 400, width: "100%" }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}
-      />
+      {currentPosition !== null ? (
+        <MapView
+          style={{ height: 400, width: "100%" }}
+          initialRegion={{
+            latitude: currentPosition.coords.latitude,
+            longitude: currentPosition.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}
+        >
+          <Marker
+            coordinate={currentPosition.coords}
+            title="Your position"
+            pinColor="red"
+            description={`near ${currentPositionAddress[0].name}`}
+          >
+            {/* <View style={{ backgroundColor: "red" }}>
+              <Text>{currentAddress[0].name}</Text>
+            </View> */}
+          </Marker>
+        </MapView>
+      ) : null}
     </View>
   );
 };
