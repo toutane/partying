357c41ec37f7f82f6d306c6c24431dd5441a357c
Charles-Antoine LEGER <ca@leger.email>
2020-04-07 22:57:58 +0200

    Cptn add : friendsflatlist

---
+++ src/components/Parties/NewParty/ParticipantsCard/GuestsList/FriendsFlatList.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { ActivityIndicator, FlatList, View } from "react-native";
+import { Item } from "./Item";
+import { screenHeight, screenWidth } from "../../../../../utils/dimensions";
+import { useSafeArea } from "react-native-safe-area-context";
+
+export const FriendsFlatList = (props) => {
+  const margin = 96 + useSafeArea().top;
+  return (
+    <FlatList
+      style={{
+        height: screenHeight,
+        // paddingHorizontal: 15,
+        paddingTop: margin,
+        paddingVertical: 10,
+      }}
+      data={props.friends}
+      renderItem={({ item, index }) => (
+        <Item
+          friend={item}
+          index={index}
+          margin={margin}
+          selected={props.guests_id.includes(item.user_id)}
+          {...props}
+        />
+      )}
+      ListFooterComponent={
+        <View
+          style={{
+            height: 130 + useSafeArea().top,
+          }}
+        >
+          {props.loading && <ActivityIndicator />}
+        </View>
+      }
+      keyExtractor={(item, index) => index.toString()}
+      onEndReached={() => props.retrieveMore(props.user.user_id)}
+      // How Close To The End Of List Until Next Data Request Is Made
+      onEndReachedThreshold={0}
+      // Refreshing (Set To True When End Reached)
+      refreshing={props.refreshing}
+    />
+  );
+};
