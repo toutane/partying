acdbc040463a3fa85fc3f5e69d5399467d8d28e0
Charles-Antoine LEGER <ca@leger.email>
2020-04-09 17:38:32 +0200

    UI ADD : adding header view of new party view

--- src/components/Parties/NewParty/Header/HeaderView.js
+++ src/components/Parties/NewParty/Header/HeaderView.js
@@ -1,78 +1,94 @@
-import React from "react";
-import { View, Animated, TouchableOpacity, Text } from "react-native";
-import { BlurView } from "expo-blur";
-import { screenWidth } from "../../../../utils/dimensions";
-import { useSafeArea } from "react-native-safe-area-context";
-import { Feather } from "@expo/vector-icons";
-import { CreateButton } from "./CreateButton";
+// import React from "react";
+// import { View, Animated, TouchableOpacity, Text } from "react-native";
+// import { BlurView } from "expo-blur";
+// import { screenWidth } from "../../../../utils/dimensions";
+// import { useSafeArea } from "react-native-safe-area-context";
+// import { Feather } from "@expo/vector-icons";
+// import { CreateButton } from "./CreateButton";
 
-export default HeaderView = props => {
+// export default HeaderView = props => {
+//   return (
+//     <View style={{ zIndex: 2, position: "absolute" }}>
+//       <Animated.View
+//         style={{
+//           zIndex: 2,
+//           position: "absolute",
+//           top: 0
+//         }}
+//       >
+//         <BlurView
+//           tint={props.theme.theme}
+//           intensity={100}
+//           style={{
+//             zIndex: 2,
+//             height: 46 + useSafeArea().top,
+//             width: screenWidth
+//           }}
+//         />
+//       </Animated.View>
+//       <Animated.View
+//         style={{
+//           zIndex: 3,
+//           height: 46 + useSafeArea().top,
+//           width: screenWidth,
+//           backgroundColor:
+//             props.isWhiteBackground !== undefined
+//               ? props.theme !== "light"
+//                 ? props.theme.gray6
+//                 : "white"
+//               : props.theme.backgroundColor,
+//           position: "absolute",
+//           top: 0,
+//           opacity: 0
+//         }}
+//       />
+//       <View
+//         style={{
+//           zIndex: 4,
+//           height: 46 + useSafeArea().top,
+//           width: screenWidth,
+//           alignItems: "center",
+//           justifyContent: "center",
+//           top: (useSafeArea().top - 4) / 2,
+//           flexDirection: "row"
+//         }}
+//       >
+//         {/* <Animated.Text
+//           style={{
+//             fontSize: 17,
+//             fontFamily: "sf-text-semibold",
+//             color: props.theme.fontColor,
+//             opacity: 1
+//           }}
+//           numberOfLines={1}
+//         >
+//           New Party
+//         </Animated.Text>
+//         <View
+//           style={{
+//             right: 0,
+//             position: "absolute"
+//           }}
+//         > */}
+//         <CreateButton {...props} />
+//         {/* </View> */}
+//       </View>
+//     </View>
+//   );
+// };
+
+import React from "react";
+import { Animated, View, Text, Image } from "react-native";
+import { TopViewCard } from "./styles";
+import { CreateButton } from "./CreateButton";
+import { useSafeArea } from "react-native-safe-area-context";
+
+export default HeaderView = (props) => {
   return (
-    <View style={{ zIndex: 2, position: "absolute" }}>
-      <Animated.View
-        style={{
-          zIndex: 2,
-          position: "absolute",
-          top: 0
-        }}
-      >
-        <BlurView
-          tint={props.theme.theme}
-          intensity={100}
-          style={{
-            zIndex: 2,
-            height: 46 + useSafeArea().top,
-            width: screenWidth
-          }}
-        />
-      </Animated.View>
-      <Animated.View
-        style={{
-          zIndex: 3,
-          height: 46 + useSafeArea().top,
-          width: screenWidth,
-          backgroundColor:
-            props.isWhiteBackground !== undefined
-              ? props.theme !== "light"
-                ? props.theme.gray6
-                : "white"
-              : props.theme.backgroundColor,
-          position: "absolute",
-          top: 0,
-          opacity: 0
-        }}
-      />
-      <View
-        style={{
-          zIndex: 4,
-          height: 46 + useSafeArea().top,
-          width: screenWidth,
-          alignItems: "center",
-          justifyContent: "center",
-          top: (useSafeArea().top - 4) / 2,
-          flexDirection: "row"
-        }}
-      >
-        {/* <Animated.Text
-          style={{
-            fontSize: 17,
-            fontFamily: "sf-text-semibold",
-            color: props.theme.fontColor,
-            opacity: 1
-          }}
-          numberOfLines={1}
-        >
-          New Party
-        </Animated.Text>
-        <View
-          style={{
-            right: 0,
-            position: "absolute"
-          }}
-        > */}
+    <TopViewCard {...props} height={46 + useSafeArea().top}>
+      <View style={{ top: 40 }}>
         <CreateButton {...props} />
-        {/* </View> */}
       </View>
-    </View>
+    </TopViewCard>
   );
 };
