61aa27dc247f39cb77d0be1f69f3897751c7b2b8
Charles-Antoine LEGER <ca@leger.email>
2020-04-12 21:54:53 +0200

    CONTEXT : put pushNotifs + appState contexts to the root

--- src/contexts/AllContexts.js
+++ src/contexts/AllContexts.js
@@ -1,37 +1,37 @@
 import React from "react";
 
-import { AppProvider } from "./AppContext";
+// import { AppProvider } from "./AppContext";
 import { UserProvider } from "./UserContext";
 import { LocationProvider } from "./LocationContext";
 import { FeedProvider } from "./FeedContext";
 import { CreatePartyProvider } from "./CreatePartyContext";
 import { PartyProvider } from "./PartyContext";
 import { FriendsProvider } from "./FriendsContext";
-import { PushNotificationsProvider } from "./PushNotificationsContext";
+// import { PushNotificationsProvider } from "./PushNotificationsContext";
 import { GuestsProvider } from "./GuestsContext";
 import { AllUsersProvider } from "./AllUsersContext";
 
 const AllContextsProvider = (props) => {
   return (
-    <AppProvider>
-      <UserProvider>
-        <LocationProvider>
-          <PushNotificationsProvider>
-            <FriendsProvider>
-              <AllUsersProvider>
-                <PartyProvider>
-                  <CreatePartyProvider>
-                    <GuestsProvider>
-                      <FeedProvider>{props.children}</FeedProvider>
-                    </GuestsProvider>
-                  </CreatePartyProvider>
-                </PartyProvider>
-              </AllUsersProvider>
-            </FriendsProvider>
-          </PushNotificationsProvider>
-        </LocationProvider>
-      </UserProvider>
-    </AppProvider>
+    // <AppProvider>
+    <UserProvider>
+      <LocationProvider>
+        {/* <PushNotificationsProvider> */}
+        <FriendsProvider>
+          <AllUsersProvider>
+            <PartyProvider>
+              <CreatePartyProvider>
+                <GuestsProvider>
+                  <FeedProvider>{props.children}</FeedProvider>
+                </GuestsProvider>
+              </CreatePartyProvider>
+            </PartyProvider>
+          </AllUsersProvider>
+        </FriendsProvider>
+        {/* </PushNotificationsProvider> */}
+      </LocationProvider>
+    </UserProvider>
+    // </AppProvider>
   );
 };
 
