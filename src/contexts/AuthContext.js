82ae88f8bc3bc4affe854d200ec20c0b9f87d55a
Charles-Antoine LEGER <ca@leger.email>
2020-04-12 21:55:14 +0200

    add all keys to new user data

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,22 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import firebase from "../firebase/Firebase";
+
+import { PushNotificationsContext } from "../contexts/PushNotificationsContext";
+
+const moment = require("moment");
 
 const AuthContext = React.createContext();
 const { Provider } = AuthContext;
 
-const AuthProvider = props => {
+const AuthProvider = (props) => {
+  const { registerForPushNotificationsAsync } = useContext(
+    PushNotificationsContext
+  );
+
   const [authenticated, setAuthenticated] = useState(false);
+
   async function register(username, email, password) {
     await firebase.auth.createUserWithEmailAndPassword(email, password);
+    let token = await registerForPushNotificationsAsync();
     await firebase.db
       .collection("users")
       .doc(firebase.auth.currentUser.uid)
       .set({
+        user_id: firebase.auth.currentUser.uid,
         username: username,
-        email: email
+        name: username,
+        email: email,
+        avatar:
+          "https://cdn0.iconfinder.com/data/icons/user-pictures/100/male-512.png",
+        friends_id: [],
+        parties_id: [],
+        bio: "It's cool to be here !",
+        register_date: moment().format(),
+        expoPushToken: token,
       });
     return firebase.auth.currentUser.updateProfile({
-      displayName: username
+      displayName: username,
     });
   }
   function login(email, password) {
@@ -33,7 +52,7 @@ const AuthProvider = props => {
         setAuthenticated,
         register,
         login,
-        logout
+        logout,
       }}
     >
       {props.children}
