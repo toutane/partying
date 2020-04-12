75129895b064504ffd1955c54b7edc1aed63d13d
Charles-Antoine LEGER <ca@leger.email>
2020-04-12 23:03:42 +0200

    implement add friends card to friends flat list

--- src/components/User/Friends/FriendsList/FriendsFlatList.js
+++ src/components/User/Friends/FriendsList/FriendsFlatList.js
@@ -1,39 +1,43 @@
 import React, { useState, useEffect } from "react";
-import { ActivityIndicator, FlatList, View } from "react-native";
+import { ActivityIndicator, FlatList, View, Text } from "react-native";
 import { Item } from "./Item";
 import { screenHeight, screenWidth } from "../../../../utils/dimensions";
 import { useSafeArea } from "react-native-safe-area-context";
+import { AddFriendsCard } from "../AddFriends/AddFriendsCard";
 
-export const FriendsFlatList = props => {
+export const FriendsFlatList = (props) => {
   // useEffect(() => console.log(props.friends), [props.friends]);
   const margin = 96 + useSafeArea().top;
   return (
-    <FlatList
-      style={{
-        height: screenHeight,
-        paddingHorizontal: 15,
-        paddingTop: margin,
-        paddingVertical: 10
-      }}
-      data={props.friends}
-      renderItem={({ item, index }) => (
-        <Item friend={item} index={index} margin={margin} {...props} />
-      )}
-      ListFooterComponent={
-        <View
-          style={{
-            height: 130 + useSafeArea().top
-          }}
-        >
-          {props.loading && <ActivityIndicator />}
-        </View>
-      }
-      keyExtractor={(item, index) => index.toString()}
-      onEndReached={() => props.retrieveMore(props.user.user_id)}
-      // How Close To The End Of List Until Next Data Request Is Made
-      onEndReachedThreshold={0}
-      // Refreshing (Set To True When End Reached)
-      refreshing={props.refreshing}
-    />
+    <View>
+      <FlatList
+        style={{
+          height: screenHeight,
+          paddingHorizontal: 15,
+          paddingTop: margin,
+          paddingVertical: 10,
+        }}
+        data={props.friends}
+        renderItem={({ item, index }) => (
+          <Item friend={item} index={index} margin={margin} {...props} />
+        )}
+        ListFooterComponent={
+          <View
+            style={{
+              height: 130 + useSafeArea().top,
+            }}
+          >
+            {props.loading && <ActivityIndicator />}
+            <AddFriendsCard />
+          </View>
+        }
+        keyExtractor={(item, index) => index.toString()}
+        onEndReached={() => props.retrieveMore(props.user.user_id)}
+        // How Close To The End Of List Until Next Data Request Is Made
+        onEndReachedThreshold={0}
+        // Refreshing (Set To True When End Reached)
+        refreshing={props.refreshing}
+      />
+    </View>
   );
 };
