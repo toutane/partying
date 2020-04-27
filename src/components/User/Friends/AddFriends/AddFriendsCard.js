39e0877cbc1ad7fa54e583f862336c09c7b21391
Charles-Antoine LEGER <ca@leger.email>
2020-04-27 22:32:04 +0200

    UI : rebuild add friends card

--- src/components/User/Friends/AddFriends/AddFriendsCard.js
+++ src/components/User/Friends/AddFriends/AddFriendsCard.js
@@ -3,13 +3,12 @@ import { View, Text, TouchableOpacity } from "react-native";
 import { Card } from "../../../Card/styles";
 import { ThemeContext } from "../../../../contexts/ThemeContext";
 import { Feather } from "@expo/vector-icons";
+import { screenWidth } from "../../../../utils/dimensions";
 
 export const AddFriendsCard = (props) => {
   const { theme } = useContext(ThemeContext);
   return (
-    <View
-      style={{ marginTop: 15, paddingHorizontal: 110, paddingVertical: 20 }}
-    >
+    <View style={{ marginTop: 20, paddingHorizontal: 10 }}>
       <TouchableOpacity
         // onPress={() => Linking.openURL("app-settings:")}
         activeOpacity={0.5}
@@ -17,28 +16,75 @@ export const AddFriendsCard = (props) => {
           borderRadius: 13,
           backgroundColor: theme.theme !== "light" ? theme.gray5 : "white",
           paddingHorizontal: 20,
-          paddingVertical: 15,
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "row",
+          paddingVertical: 20,
         }}
       >
         <Text
           style={{
-            marginRight: 10,
-            color: theme.green,
+            color: theme.gray3,
             fontSize: 17,
-            fontFamily: "sf-text-regular",
+            fontFamily: "sf-display-semibold",
           }}
         >
-          Add friends (because Partying is better with friends)
+          This place seems a little empty...
         </Text>
-        <Feather
-          color={theme.green}
-          size={20}
-          name="user-plus"
-          style={{ top: -1 }}
-        />
+        <Text
+          style={{
+            marginTop: 15,
+            fontSize: 21,
+            color: theme.fontColor,
+            fontFamily: "sf-display-semibold",
+          }}
+        >
+          Invite a friend to join Partying or add one already registered :
+        </Text>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginTop: 20,
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              width: (screenWidth - 110) / 2,
+              paddingVertical: 5,
+              backgroundColor: theme.green,
+              alignItems: "center",
+              borderRadius: 8,
+            }}
+          >
+            <Text
+              style={{
+                color: theme.backgroundColor,
+                fontFamily: "sf-display-semibold",
+                fontSize: 17,
+              }}
+            >
+              Invite a friend
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              width: (screenWidth - 110) / 2,
+              paddingHorizontal: 20,
+              paddingVertical: 5,
+              backgroundColor: theme.green,
+              alignItems: "center",
+              borderRadius: 8,
+            }}
+          >
+            <Text
+              style={{
+                color: theme.backgroundColor,
+                fontFamily: "sf-display-semibold",
+                fontSize: 17,
+              }}
+            >
+              Add a friend
+            </Text>
+          </TouchableOpacity>
+        </View>
       </TouchableOpacity>
     </View>
   );
