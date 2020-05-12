5484a35ed5ba68d2dd8855b2533f23cd8f9c9909
Charles-Antoine LEGER <ca@leger.email>
2020-05-12 22:04:46 +0200

    UI : refactoring register view

--- src/components/views/RegisterView.js
+++ src/components/views/RegisterView.js
@@ -1,128 +1,44 @@
 import React, { useState, useContext } from "react";
-import { View, Text, Button, TextInput } from "react-native";
-import { useSafeArea } from "react-native-safe-area-context";
+import { View, Keyboard, TouchableOpacity, ScrollView } from "react-native";
+import { SvgXml } from "react-native-svg";
 
 import { ThemeContext } from "../../contexts/ThemeContext";
-import { AuthContext } from "../../contexts/AuthContext";
+
+import Title from "../OnBoarding/CreateAccount/Title";
+import SignUpCard from "../OnBoarding/CreateAccount/SignUpCard";
+
+import { undraw_festivities } from "../../../assets/svg/undraw_festivities";
+import { fireworks_city_party_dark } from "../../../assets/svg/fireworks_city_party_dark";
 
 export default RegisterView = (props) => {
-  const { register, login } = useContext(AuthContext);
   const { theme } = useContext(ThemeContext);
-
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("azerty");
-
   return (
-    <View
+    <TouchableOpacity
+      activeOpacity={1}
       style={{
         flex: 1,
-        // justifyContent: "center",
-        alignItems: "center",
         backgroundColor: theme.backgroundColor,
+        alignItems: "center",
       }}
+      onPress={Keyboard.dismiss}
     >
-      <Text
-        style={{
-          position: "absolute",
-          top: 46 + useSafeArea().top,
-          left: 25,
-          fontSize: 34,
-          fontFamily: "sf-display-bold",
-          color: theme.fontColor,
-        }}
-      >
-        Register
-      </Text>
-      <View style={{ top: 250 }}>
-        <TextInput
-          style={{
-            borderRadius: 13,
-            height: 50,
-            width: 300,
-            borderWidth: 2,
-            fontFamily: "sf-text-regular",
-            fontSize: 20,
-            borderColor: theme.green,
-            paddingHorizontal: 15,
-            color: theme.fontColor,
-          }}
-          autoCapitalize="none"
-          autoFocus={true}
-          placeholder="username"
-          returnKeyType="next"
-          onSubmitEditing={() => emailInput.focus()}
-          onChangeText={setUsername}
-          selectionColor={"#1DC161"}
-          placeholderTextColor={
-            theme.theme === "light" ? theme.gray2 : theme.gray3
-          }
+      <Title {...props} theme={theme} />
+      {theme.theme === "light" ? (
+        <SvgXml
+          xml={undraw_festivities}
+          width="230%"
+          height="120%"
+          style={{ position: "absolute", left: -200 }}
         />
-        <TextInput
-          style={{
-            borderRadius: 13,
-            height: 50,
-            width: 300,
-            borderWidth: 2,
-            fontFamily: "sf-text-regular",
-            fontSize: 20,
-            borderColor: theme.green,
-            paddingHorizontal: 15,
-            marginTop: 30,
-            color: theme.fontColor,
-          }}
-          autoCapitalize="none"
-          placeholder="email"
-          keyboardType="email-address"
-          returnKeyType="next"
-          ref={(input) => (emailInput = input)}
-          onSubmitEditing={() => passwordInput.focus()}
-          onChangeText={setEmail}
-          selectionColor={"#1DC161"}
-          placeholderTextColor={
-            theme.theme === "light" ? theme.gray2 : theme.gray3
-          }
+      ) : (
+        <SvgXml
+          xml={fireworks_city_party_dark}
+          width="230%"
+          height="120%"
+          style={{ position: "absolute", left: -200 }}
         />
-        <TextInput
-          style={{
-            borderRadius: 13,
-            height: 50,
-            width: 300,
-            borderWidth: 2,
-            fontFamily: "sf-text-regular",
-            fontSize: 20,
-            borderColor: theme.green,
-            paddingHorizontal: 15,
-            marginTop: 30,
-            marginBottom: 30,
-            color: theme.fontColor,
-          }}
-          autoCapitalize="none"
-          placeholder="password"
-          secureTextEntry
-          returnKeyType="go"
-          ref={(input) => (passwordInput = input)}
-          onChangeText={setPassword}
-          selectionColor={"#1DC161"}
-          placeholderTextColor={
-            theme.theme === "light" ? theme.gray2 : theme.gray3
-          }
-          onSubmitEditing={() =>
-            register(username, email, password).catch((error) =>
-              console.log(error)
-            )
-          }
-        />
-        <Button
-          title="Register"
-          color={theme.green}
-          onPress={() =>
-            register(username, email, password).catch((error) =>
-              console.log(error)
-            )
-          }
-        />
-      </View>
-    </View>
+      )}
+      <SignUpCard {...props} theme={theme} />
+    </TouchableOpacity>
   );
 };
